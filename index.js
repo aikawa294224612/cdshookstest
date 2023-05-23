0b6872c52983cbe22e7c90ea0019f29b327a5d1a
Polly Peng <39365400+aikawa294224612@users.noreply.github.com>
2023-05-23 10:03:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
-const axios = require('axios');
 
 const app = express();
 const port = 3000;
